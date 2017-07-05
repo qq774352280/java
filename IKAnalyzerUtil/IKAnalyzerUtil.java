ae2467b624f4668e26dc92e61dc890fea9c5e2db
qq774352280 <774352280@qq.com>
2017-07-05 17:06:11 +0800

    Update IKAnalyzerUtil.java

--- IKAnalyzerUtil/IKAnalyzerUtil.java
+++ IKAnalyzerUtil/IKAnalyzerUtil.java
@@ -1,1 +1,26 @@
+public class IKAnalyzerUtil {
+	
+	public static List<String> participles(String s){
+		if("".equals(s)){
+			return null;
+		}
+		IKAnalyzer ana=new IKAnalyzer(true);
+		
+		List<String> list = new ArrayList<String>();
+		StringReader reader=new StringReader(s);
+		
+		//分词  
+		TokenStream ts=ana.tokenStream("", reader);
+		CharTermAttribute term=ts.getAttribute(CharTermAttribute.class);
+		//遍历分词数据  
+		try {
+			while(ts.incrementToken()){  
+				list.add(term.toString());
+			}
+		} catch (IOException e) {
+		}
+		reader.close();
+		return list;
+	}
 
+}
